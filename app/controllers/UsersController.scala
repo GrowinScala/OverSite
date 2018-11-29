156b9db97d976a63d0f096f47fa377d964fd73ae
Pedro Luis <pluis@growin.pt>
2018-11-29 17:47:35 +0000

    some experimental routes added

---
+++ app/controllers/UsersController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+
+class UsersController @Inject()(cc: ControllerComponents) (implicit assetsFinder: AssetsFinder)
+  extends AbstractController(cc) {
+
+  /**
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/users`.
+    */
+  def users = Action {
+    Ok(views.html.index("Your user account is created"))
+  }
+
+}
