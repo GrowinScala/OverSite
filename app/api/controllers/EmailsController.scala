96bf1b8ac37f6f4cdf3c8a3cf462bec4f4584120
Pedro Luis <pluis@growin.pt>
2018-12-03 17:51:59 +0000

    Uma merda

--- app/api/controllers/EmailsController.scala
+++ app/api/controllers/EmailsController.scala
@@ -1,52 +1,50 @@
 package api.controllers
 
+import scala.concurrent.duration.FiniteDuration
 import javax.inject._
-
 import akka.actor.ActorSystem
+import database.mappings.TablesMysql.Email
+import play.api.libs.json.{JsResult, JsSuccess, JsValue}
 import play.api.mvc._
-import slick.jdbc.MySQLProfile.api._
+import play.api.libs.json.Json
 
 import scala.concurrent.duration._
-import scala.concurrent._
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
-
-/**
-  * This controller creates an `Action` that demonstrates how to write
-  * simple asynchronous code in a controller. It uses a timer to
-  * asynchronously delay sending a response for 1 second.
-  *
-  * @param cc standard controller components
-  * @param actorSystem We need the `ActorSystem`'s `Scheduler` to
-  * run code after a delay.
-  * @param exec We need an `ExecutionContext` to execute our
-  * asynchronous code.  When rendering content, you should use Play's
-  * default execution context, which is dependency injected.  If you are
-  * using blocking operations, such as database or network access, then you should
-  * use a different custom execution context that has a thread pool configured for
-  * a blocking API.
-  */
 @Singleton
-class EmailsController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends AbstractController(cc) {
-  val db = Database.forConfig("mysql")
+class EmailsController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem) (implicit exec: ExecutionContext)
+  extends AbstractController(cc) {
 
-  /**
-    * Creates an Action that returns a plain text message after a delay
-    * of 1 second.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/message`.
-    */
-  /*def emails = Action.async {
-    getFutureEmail(1.second).map { msg => Ok(msg) }
+  def reads(json: JsValue): JsResult[Email] = {
+    val emailID = (json \ "emailID").as[String]
+    val chatID = (json \ "chatID").as[String]
+    val fromAdress = (json \ "fromAdress").as[String]
+    val dateOF = (json \ "dateOF").as[String]
+    val header = (json \ "header").as[String]
+    val body = (json \ "body").as[String]
+
+    JsSuccess(Email(emailID,chatID,fromAdress,dateOF,header,body))
   }
 
-  private def getFutureMessage(delayTime: FiniteDuration): Future[String] = {
-    val promise: Promise[String] = Promise[String]()
-    actorSystem.scheduler.scheduleOnce(delayTime) {
-      promise.success("Hi!")
-    }(actorSystem.dispatcher) // run scheduled tasks using the actor system's dispatcher
-    promise.future
-  }*/
+  /**
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/users`.
+    */
+
+  def index =  Action{
+    Ok("something")
+  }
+
+
+  def email(chatID : String) = Action { request =>
+    val json = request.body.asJson.get
+    //val email = json.as[Email]
+    val email = reads(json)
+    print(email)
+    Ok
+  }
+
 
 }
