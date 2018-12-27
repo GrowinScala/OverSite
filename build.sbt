44a4528075d89e7002aa8cce3f39f2ef166e28dc
RuiGSValente <ruiguilhermevalente@gmail.com>
2018-12-27 17:40:17 +0000

    Tests started. First test of T19.6: Test Suite for class User Repository

--- build.sbt
+++ build.sbt
@@ -5,19 +5,17 @@ version := "1.0-SNAPSHOT"
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 resolvers += Resolver.sonatypeRepo("snapshots")
-resolvers += "Artima Maven Repository" at "http://repo.artima.com/releases"
 
 scalaVersion := "2.12.6"
 
 crossScalaVersions := Seq("2.11.12", "2.12.7")
 
 libraryDependencies += guice
-libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" 
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"
+libraryDependencies += "com.h2database" % "h2" % "1.4.196" % Test
 
 libraryDependencies ++= Seq(
   "com.typesafe.slick" %% "slick" % "3.2.0",
   "com.typesafe.slick" %% "slick-hikaricp" % "3.2.0",
   "mysql" % "mysql-connector-java" % "latest.integration"
 )
-
-logBuffered in Test := false
---
+++ test/repository/actions/UserActions.scala
@@ -0,0 +1,36 @@
+package repository.actions
+
+import api.dtos.CreateUserDTO
+import database.mappings.UserMappings._
+import database.repository.UserRepository
+import javax.inject.Inject
+import slick.jdbc.H2Profile.api._
+
+import scala.concurrent.ExecutionContext
+
+class UserActions @Inject() (implicit val executionContext: ExecutionContext, implicit val db: Database) {
+
+  val userActions = new UserRepository()
+
+  def createFilesTable = {
+    db.run(loginTable.schema.create)
+    db.run(userTable.schema.create)
+  }
+
+  def dropFilesTable = {
+    db.run(loginTable.schema.drop)
+    db.run(userTable.schema.drop)
+  }
+
+  def deleteRowsTable = {
+    db.run(loginTable.delete)
+    db.run(userTable.delete)
+  }
+
+  def insertUserTest(user: CreateUserDTO) = {
+    userActions.insertUser(user)
+    val userFilter = userTable.filter(_.username === user.username).filter(_.password === user.password).result
+    db.run(userFilter).map(_.nonEmpty)
+  }
+
+}
