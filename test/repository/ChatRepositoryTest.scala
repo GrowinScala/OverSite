1d3ddd786d78af062d2d197ebe4591a34f102dc9
RuiGSValente <ruiguilhermevalente@gmail.com>
2019-01-07 18:20:16 +0000

    T19.4: Test Suite for class Chat Repository (NOT FINISHED)

--- test/repository/ChatRepositoryTest.scala
+++ test/repository/ChatRepositoryTest.scala
@@ -4,7 +4,7 @@ import api.dtos.{ CreateEmailDTO, CreateUserDTO }
 import database.mappings.ChatMappings.chatTable
 import database.mappings.EmailMappings.{ bccTable, ccTable, emailTable, toAddressTable }
 import database.mappings.UserMappings.{ loginTable, userTable }
-import database.repository.{ ChatRepository, ChatRepositoryImpl }
+import database.repository.{ ChatRepository, ChatRepositoryImpl, EmailRepositoryImpl }
 import org.scalatest._
 import play.api.Mode
 import play.api.inject.Injector
@@ -21,7 +21,11 @@ class ChatRepositoryTest extends WordSpec with BeforeAndAfterAll with BeforeAndA
   lazy implicit val db: Database = injector.instanceOf[Database]
   lazy implicit val rep = new ChatRepositoryImpl()
 
+  val emailActions = new EmailRepositoryImpl()
+  val chatActions = new ChatRepositoryImpl()
+
   val userCreation = new CreateUserDTO("rvalente@growin.com", "12345")
+  val userCreationWrongUser = new CreateUserDTO("pluis@growin.com", "12345")
   val emailCreation = new CreateEmailDTO(
     Option("123"),
     "2025-10-10",
@@ -47,5 +51,48 @@ class ChatRepositoryTest extends WordSpec with BeforeAndAfterAll with BeforeAndA
   }
 
   /* Verify if a chat is inserted in database */
+  "ChatRepository #insertChat" should {
+    "check if the chat is inserted in the chat table correctly" in {
+      val resultchatID = Await.result(emailActions.insertEmail(userCreation.username, emailCreation), Duration.Inf)
+      Await.result(chatActions.insertChat(emailCreation, resultchatID), Duration.Inf)
+      val resultChatTable = Await.result(db.run(chatTable.result), Duration.Inf)
+      /** Verify if something was inserted in the chat table*/
+      assert(resultChatTable.nonEmpty)
+
+      /** Verify if the chatID in chat Table matches with email inserted */
+      resultChatTable.map(row => assert(row.chatID === resultchatID))
+
+      /** Verify if the header in chat Table matches with email inserted */
+      resultChatTable.map(row => assert(row.header === emailCreation.header))
+    }
+  }
+
+  /* Verify if a chat is inserted in database */
+  "ChatRepository #getInbox" should {
+    "check if the Inbox has the right messages for an specific user" in {
+      val resultchatID = Await.result(emailActions.insertEmail(userCreation.username, emailCreation), Duration.Inf)
+      val resultInbox = Await.result(chatActions.getInbox(userCreation.username), Duration.Inf)
+
+      /** Verify if Inbox is not empty*/
+      assert(resultInbox.nonEmpty)
+
+      /** Verify if the chatID in chat Table matches with email inserted */
+      resultInbox.map(row => assert(row._1 === resultchatID))
+
+      /** Verify if the header in chat Table matches with email inserted */
+      resultInbox.map(row => assert(row._2 === emailCreation.header))
+    }
+  }
+
+  /* Verify if a chat is inserted in database */
+  "ChatRepository #getInbox" should {
+    "check if the Inbox is empty for an user without messages" in {
+      val resultchatID = Await.result(emailActions.insertEmail(userCreation.username, emailCreation), Duration.Inf)
+      val resultInbox = Await.result(chatActions.getInbox(userCreationWrongUser.username), Duration.Inf)
+
+      /** Verify if Inbox is not empty*/
+      assert(resultInbox.isEmpty)
+    }
+  }
 
 }
