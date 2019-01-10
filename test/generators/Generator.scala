b81289a64472909d790c54be8a96079d158df817
RuiGSValente <ruiguilhermevalente@gmail.com>
2019-01-10 11:13:56 +0000

    GENERATOR APPLIED TO ALL TESTS

--- test/generators/Generator.scala
+++ test/generators/Generator.scala
@@ -9,29 +9,33 @@ class Generator extends CommonDTOGenerator {
   lazy val token: String = uuid
   lazy val username: String = emailAddress
 
+  /** Generates a random password that varies between 1 and 20 alphanumeric strings*/
   def password: String = {
     val passwordAux: String = alphaNumStr
     val numberPassword: Int = choose(1, 20)
     transformEmail(passwordAux).take(numberPassword)
   }
 
+  /** Generates a random email address that varies between 1 and 10 alphanumeric strings + "@growin.pt"*/
   def emailAddress: String = {
     val emailAddressAux: String = alphaNumStr
     val numberEmailAddress: Int = choose(1, 10)
     emailAddressAux.take(numberEmailAddress) + "@growin.pt"
   }
 
-  def emailAddressesSeq(num: Int): Array[String] = {
-
-    if (num == 0) Array()
+  /** Creates a sequence of n random email addresses */
+  def emailAddressesSeq(num: Int): Seq[String] = {
+    if (num == 0) Seq()
     else emailAddressesSeq(num - 1) :+ emailAddress
   }
 
-  def emailAddresses: Array[String] = {
+  /** Generates a Sequence of emailAddresses that varies between 1 and 5**/
+  def emailAddresses: Seq[String] = {
     val emailNumberAux: Int = choose(1, 5)
     emailAddressesSeq(emailNumberAux)
   }
 
+  /** Generates a string with date format**/
   def dateOf: String = {
     val dateOfYear: Int = choose(1999, 2099)
     val dateOfMonth: Int = choose(1, 12)
@@ -39,13 +43,16 @@ class Generator extends CommonDTOGenerator {
     s"$dateOfYear-$dateOfMonth-$dateOfDay"
   }
 
+  /** Shuffle a list of the most common 100 words*/
   def words: List[String] = Random.shuffle(listMostCommonWords)
 
+  /** Generates a string with sentence format, containing around 2 and 5 words */
   def header: String = {
     val headerNumberAux: Int = choose(2, 5)
     words.take(headerNumberAux).foldLeft("")((string, word) => string + " " + word)
   }
 
+  /** Generates a string with sentence format, containing around 5 and 20 words */
   def body: String = {
     val bodyNumberAux: Int = choose(5, 20)
     val bodySentenceAux: List[String] = Random.shuffle(listMostCommonWords).take(bodyNumberAux)
