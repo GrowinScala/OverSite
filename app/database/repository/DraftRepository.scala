8df7e2a110c34affbbdd0ef84f677f47a844b65c
RuiGSValente <ruiguilhermevalente@gmail.com>
2019-01-23 11:29:22 +0000

    TRAIT OF DRAFTS FIXED

--- app/database/repository/DraftRepository.scala
+++ app/database/repository/DraftRepository.scala
@@ -6,13 +6,13 @@ import scala.concurrent.Future
 
 trait DraftRepository {
 
-  def updateDraft(draft: CreateEmailDTO, username: String, draftID: String): Future[String]
   def insertDraft(username: String, draft: CreateEmailDTO): Future[String]
-  def takeDraftMakeSent(username: String, draftID: String, listCCs: Seq[String], listBCCs: Seq[String], listTos: Seq[String]): Future[String]
+  def getDrafts(userEmail: String, isTrash: Boolean): Future[Seq[MinimalInfoDTO]]
+  def updateDraft(draft: CreateEmailDTO, username: String, draftID: String): Future[String]
+  def getDraft(userEmail: String, isTrash: Boolean, draftID: String): Future[Seq[DraftInfoDTO]]
+  def takeDraftMakeSent(username: String, draftID: String, listTos: Seq[String], listBCCs: Seq[String], listCCs: Seq[String]): Future[String]
   def destinations(username: String, draftID: String): Future[(Seq[String], Seq[String], Seq[String])]
   def hasDestination(listTos: Seq[String], listBCCs: Seq[String], listCCs: Seq[String]): Future[Boolean]
-
-  def getDrafts(userEmail: String, isTrash: Boolean): Future[Seq[MinimalInfoDTO]]
-  def getDraft(userEmail: String, isTrash: Boolean, draftID: String): Future[Seq[DraftInfoDTO]]
+  def moveInOutTrash(userEmail: String, draftID: String, trash: Boolean): Future[Int]
 
 }
